bdc6ea080c8debe7144bac93b41cd08fc3e13839
Brian Hackett <bhackett1024@gmail.com>
2022-02-15 12:08:41 -0800

    Add recordreplay/puppeteer as a dependency

--- examples/whatsmyuseragent.js
+++ examples/whatsmyuseragent.js
@@ -4,6 +4,6 @@ const puppeteer = require("@recordreplay/puppeteer");
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("http://whatsmyuseragent.org");
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve, 2000));
   await browser.close();
 })();
